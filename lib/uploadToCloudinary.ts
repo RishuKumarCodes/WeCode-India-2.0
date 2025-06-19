21ad053d0654e93630846b3277667a7e2fa02380
Dipanshu Zalke <dipanshuzalke@gmail.com>
2025-06-20 02:23:16 +0530

    Doubt forum done

--- lib/uploadToCloudinary.ts
+++ lib/uploadToCloudinary.ts
@@ -3,7 +3,7 @@ export async function uploadToCloudinary(
 ): Promise<{ url: string; type: "image" | "video" }> {
   const formData = new FormData();
   formData.append("file", file);
-  formData.append("upload_preset", "unsigned_preset");
+  formData.append("upload_preset", process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!);
   formData.append("folder", "WeCode_India");
   console.log("Uploading to:", process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME);
 
